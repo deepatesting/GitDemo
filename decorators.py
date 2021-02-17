f6da1921bda3798381f8ed0d20b5cd33c3cab9c5
kspdeepa82 <kspdeepa82@gmail.com>
2021-02-17 17:59:51 +0000

    First commit - GitX

---
+++ decorators.py
@@ -0,0 +1,50 @@
+
+import functools
+
+user = {"username": "Rob", "access_level": "admin"}
+
+def make_secure(func):
+    # print(func.__code__.co_argcount)
+    # @functools.wraps(func)
+    def secure_function():
+        if user["access_level"] == "admin":
+            return func()
+        else:
+            return f"No admin access for {user['username']}."
+    return secure_function
+
+@make_secure
+def get_admin_passwd():
+    """This is get_admin_passwd"""
+    return "1234"
+
+# get_admin_passwd = make_secure(get_admin_passwd)
+
+print(get_admin_passwd())
+print(get_admin_passwd.__name__)
+print(get_admin_passwd.__doc__)
+
+
+"""
+def outer_function(x):
+    # Hidden from the outer code
+    def inner_increment(y):
+        print("calling..inner func")
+        return x(y)
+    print("Calling...outer func")
+    return inner_increment
+
+# @outer_function
+# calc = outer_function(calc)
+def calc(y):
+    print("I am calc....\n", y)
+
+
+# calc = outer_function(calc)
+# calc("GOOD")
+
+
+
+funct_return = outer_function(calc)
+funct_return("GOOD")
+"""
